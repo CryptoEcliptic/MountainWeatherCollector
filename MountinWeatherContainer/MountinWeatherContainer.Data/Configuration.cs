1cc7d026931e0997e363e364581aa35cccb2a4d0
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-04-17 20:12:05 +0300

    Revoved Configuration class

--- MountinWeatherContainer/MountinWeatherContainer.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MountinWeatherContainer.Data
-{
-    public class Configuration
-    {
-       public static readonly string ConnectionString = @"Server=NAME-PC\SQLEXPRESS; Database=MountainWeather; Integrated Security=true";
-    }
-}
